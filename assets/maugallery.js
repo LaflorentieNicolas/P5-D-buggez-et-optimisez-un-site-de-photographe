a0cdbe9182f6e90b06cd061ae5934f9be6467531
Laflorentie Nicolas <laflorentie.nicolas@gmail.com>
2024-03-13 12:33:34 +0100

    maugallery.js minifier

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -12,7 +12,6 @@
         );
       }
       $.fn.mauGallery.listeners(options);
-
       $(this)
         .children(".gallery-item")
         .each(function (index) {
@@ -28,7 +27,6 @@
             tagsCollection.push(theTag);
           }
         });
-
       if (options.showTags) {
         $.fn.mauGallery.methods.showItemTags(
           $(this),
@@ -36,17 +34,16 @@
           tagsCollection
         );
       }
-
       $(this).fadeIn(500);
     });
   };
   $.fn.mauGallery.defaults = {
     columns: 3,
-    lightBox: true,
+    lightBox: !0,
     lightboxId: null,
-    showTags: true,
+    showTags: !0,
     tagsPosition: "bottom",
-    navigation: true,
+    navigation: !0,
   };
   $.fn.mauGallery.listeners = function (options) {
     $(".gallery-item").on("click", function () {
@@ -56,7 +53,6 @@
         return;
       }
     });
-
     $(".gallery").on("click", ".nav-link", $.fn.mauGallery.methods.filterByTag);
     $(".gallery").on("click", ".mg-prev", () =>
       $.fn.mauGallery.methods.prevImage(options.lightboxId)
@@ -138,13 +134,11 @@
       }
       let index = 0,
         next = null;
-
       $(imagesCollection).each(function (i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
           index = i;
         }
       });
-      //correction le -1 doit s'appliquer ici
       next =
         imagesCollection[index - 1] ||
         imagesCollection[imagesCollection.length - 1];
@@ -174,13 +168,11 @@
       }
       let index = 0,
         next = null;
-
       $(imagesCollection).each(function (i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
           index = i;
         }
       });
-      //Correction index + 1
       next = imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -215,7 +207,6 @@
                 <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
       });
       var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
-
       if (position === "bottom") {
         gallery.append(tagsRow);
       } else if (position === "top") {
@@ -230,9 +221,7 @@
       }
       $(".active-tag").removeClass("active active-tag");
       $(this).addClass("active-tag");
-
       var tag = $(this).data("images-toggle");
-
       $(".gallery-item").each(function () {
         $(this).parents(".item-column").hide();
         if (tag === "all") {
